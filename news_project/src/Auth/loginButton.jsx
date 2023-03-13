67b19a7937820840f17ed141f0183cfa0d9f046a
Shahar Tsuk <shahartzuk23@gmail.com>
2023-03-13 12:17:25 +0200

    falling-stars

--- news_project/src/Auth/loginButton.jsx
+++ news_project/src/Auth/loginButton.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import "../Auth/style.css";
+import "../Style/authPages.css";
+import { FallingStars } from "../Components/fallingStars";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
@@ -8,6 +9,7 @@ const LoginButton = () => {
   return( 
   
     <div className="firstPage">
+      <FallingStars/>
       <h1>Welcome To My First News Flashes Web</h1>
   <button className="btn btn-primary" onClick={() => loginWithRedirect("http://localhost:3000/")}>Log In</button>
   </div>
---
+++ news_project/src/Components/fallingStars.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "../Style/fallingStar.css";
+export const FallingStars = () => {
+  return (
+    <div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+    </div>
+  );
+};
