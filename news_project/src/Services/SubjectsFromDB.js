635452adb86ef6d3a84ad657844766786d882e4a
BarRachamin <bar.rachamin15@gmail.com>
2023-03-13 21:05:25 +0200

    continue Send 3 Subjects

--- news_project/src/Services/SubjectsFromDB.js
+++ news_project/src/Services/SubjectsFromDB.js
@@ -1,13 +1,23 @@
 import axios from "axios";
 
-const EndPoint = "https://localhost:7094/"; 
+const EndPoint = "https://localhost:7094/api/"; 
 
-// send BusinessCompany user to server and add to db
+// get subjuects from DB
 export const GetSubjectsFromServer = async () => {
   try {
-    let response = await axios.get(`${EndPoint}api/Subjects/GetSubjects`);
+    let response = await axios.get(`${EndPoint}Subjects/GetSubjects`);
     if (response.status === 200) return response.data;
   } catch (error) {
     console.log(error);
+  }
+};
+
+
+//send 3 subjects to db
+export const Choose3Subjects = async (UserDetails) => {
+  try {
+    await axios.post(`${EndPoint}`, UserDetails);
+  } catch (error) {
+    console.error(error);
   }
 };
