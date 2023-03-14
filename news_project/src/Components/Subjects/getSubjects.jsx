cdd836c3f580ae0fc8320e4c5c45f3ceace928b8
BarRachamin <bar.rachamin15@gmail.com>
2023-03-14 17:29:47 +0200

    Choose 3 Subjects

--- news_project/src/Components/Subjects/getSubjects.jsx
+++ news_project/src/Components/Subjects/getSubjects.jsx
@@ -1,3 +1,4 @@
+
 import React, { useEffect, useState } from "react";
 import { GetSubjectsFromServer } from "../../Services/SubjectsFromDB";
 
@@ -5,25 +6,52 @@ import "../../Style/subjects.css";
 
 export const GetSubjects = () => {
   const [Subjects, setSubjects] = useState([]);
-
+  const [checkedList, setCheckedList] = useState([]);
   const HandleSubjects = async () => {
     let subs = await GetSubjectsFromServer();
     setSubjects(subs);
+  };
+  const handleSelect = (event) => {
+    const value = event.target.value;
+    const isChecked = event.target.checked;
+    console.log("value:",value);
+    console.log(isChecked);
+
+
+    if (isChecked) {
+      //Add checked item into checkList
+      setCheckedList([...checkedList, value]);
+    } else {
+      //Remove unchecked item from checkList
+      const filteredList = checkedList.filter((item) => item !== value);
+      setCheckedList(filteredList);
+    }
   };
   useEffect(() => {
     HandleSubjects();
   }, []);
   console.log(Subjects);
+  console.log(checkedList);
   return (
-    <div className="subjects-container">
+    <div>
+    <div  className="subjects-container" >
       {Subjects.length > 0 &&
         Subjects.map((s) => {
           return (
-            <button className="btn btn-lg btn-primary btn-subject-size">
-              {s.name}
-            </button>
+                  <div className="card" >
+                  <div key={s.id} className="card-body">
+                    <h5 className="card-title">{s.name}</h5>
+                    <input
+                      type="checkbox"
+                      value={s.name}
+                      onChange={(event)=>handleSelect(event)}
+                    />
+                  </div>
+              </div>
           );
         })}
+        <button className="btn btn-secondary" disabled={checkedList.length>3 || checkedList.length<=0 } >Save</button>
+        </div>
     </div>
   );
 };
